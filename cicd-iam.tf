cb4188802c4a91f76106833d505d1219791f8675
satyaposwal <100473179+satyaposwal@users.noreply.github.com>
2022-04-18 16:27:18 +0530

    Update cicd-iam.tf

--- cicd-iam.tf
+++ cicd-iam.tf
@@ -1,114 +1,95 @@
-resource "aws_s3_bucket" "codepipeline_artifacts_sanyam975" {
-  bucket = "pipeline-artifacts-sanyam975"
-} 
+resource "aws_iam_role" "tf-codepipeline-role" {
+  name = "tf-codepipeline-role"
 
-resource "aws_codebuild_project" "tf-plan" {
-  name          = "tf-cicd-plan2"
-  description   = "Plan stage for terraform"
-  service_role  = aws_iam_role.tf-codebuild-role.arn
-
-  artifacts {
-    type = "CODEPIPELINE"
-  }
-
-  environment {
-    compute_type                = "BUILD_GENERAL1_SMALL"
-    image                       = "hashicorp/terraform:0.14.3"
-    type                        = "LINUX_CONTAINER"
-    image_pull_credentials_type = "SERVICE_ROLE"
-    registry_credential{
-        credential = var.dockerhub_credentials
-        credential_provider = "SECRETS_MANAGER"
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": "sts:AssumeRole",
+      "Principal": {
+        "Service": "codepipeline.amazonaws.com"
+      },
+      "Effect": "Allow",
+      "Sid": ""
     }
- }
- source {
-     type   = "CODEPIPELINE"
-     buildspec = file("buildspec/plan-buildspec.yml")
- }
+  ]
+}
+EOF
+
 }
 
-resource "aws_codebuild_project" "tf-apply" {
-  name          = "tf-cicd-apply"
-  description   = "Apply stage for terraform"
-  service_role  = aws_iam_role.tf-codebuild-role.arn
-
-  artifacts {
-    type = "CODEPIPELINE"
-  }
-
-  environment {
-    compute_type                = "BUILD_GENERAL1_SMALL"
-    image                       = "hashicorp/terraform:0.14.3"
-    type                        = "LINUX_CONTAINER"
-    image_pull_credentials_type = "SERVICE_ROLE"
-    registry_credential{
-        credential = var.dockerhub_credentials
-        credential_provider = "SECRETS_MANAGER"
+data "aws_iam_policy_document" "tf-cicd-pipeline-policies" {
+    statement{
+        sid = ""
+        actions = ["codestar-connections:UseConnection"]
+        resources = ["*"]
+        effect = "Allow"
     }
- }
- source {
-     type   = "CODEPIPELINE"
-     buildspec = file("buildspec/apply-buildspec.yml")
- }
+    statement{
+        sid = ""
+        actions = ["cloudwatch:*", "s3:*", "codebuild:*"]
+        resources = ["*"]
+        effect = "Allow"
+    }
+}
+
+resource "aws_iam_policy" "tf-cicd-pipeline-policy" {
+    name = "tf-cicd-pipeline-policy"
+    path = "/"
+    description = "Pipeline policy"
+    policy = data.aws_iam_policy_document.tf-cicd-pipeline-policies.json
+}
+
+resource "aws_iam_role_policy_attachment" "tf-cicd-pipeline-attachment" {
+    policy_arn = aws_iam_policy.tf-cicd-pipeline-policy.arn
+    role = aws_iam_role.tf-codepipeline-role.id
 }
 
 
-resource "aws_codepipeline" "cicd_pipeline" {
+resource "aws_iam_role" "tf-codebuild-role" {
+  name = "tf-codebuild-role"
 
-    name = "tf-cicd"
-    role_arn = aws_iam_role.tf-codepipeline-role.arn
-
-    artifact_store {
-        type="S3"
-        location = aws_s3_bucket.codepipeline_artifacts_sanyam975.id
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": "sts:AssumeRole",
+      "Principal": {
+        "Service": "codebuild.amazonaws.com"
+      },
+      "Effect": "Allow",
+      "Sid": ""
     }
+  ]
+}
+EOF
 
-    stage {
-        name = "Source"
-        action{
-            name = "Source"
-            category = "Source"
-            owner = "AWS"
-            provider = "CodeStarSourceConnection"
-            version = "1"
-            output_artifacts = ["tf-code"]
-            configuration = {
-                FullRepositoryId = "satyaposwal/aws-cicd-pipeline"
-                BranchName   = "master"
-                ConnectionArn = var.codestar_connector_credentials
-                OutputArtifactFormat = "CODE_ZIP"
-            }
-        }
+}
+
+data "aws_iam_policy_document" "tf-cicd-build-policies" {
+    statement{
+        sid = ""
+        actions = ["logs:*", "s3:*", "codebuild:*", "secretsmanager:*","iam:*"]
+        resources = ["*"]
+        effect = "Allow"
     }
+}
 
-    stage {
-        name ="Plan"
-        action{
-            name = "Build"
-            category = "Build"
-            provider = "CodeBuild"
-            version = "1"
-            owner = "AWS"
-            input_artifacts = ["tf-code"]
-            configuration = {
-                ProjectName = "tf-cicd-plan"
-            }
-        }
-    }
+resource "aws_iam_policy" "tf-cicd-build-policy" {
+    name = "tf-cicd-build-policy"
+    path = "/"
+    description = "Codebuild policy"
+    policy = data.aws_iam_policy_document.tf-cicd-build-policies.json
+}
 
-    stage {
-        name ="Deploy"
-        action{
-            name = "Deploy"
-            category = "Build"
-            provider = "CodeBuild"
-            version = "1"
-            owner = "AWS"
-            input_artifacts = ["tf-code"]
-            configuration = {
-                ProjectName = "tf-cicd-apply"
-            }
-        }
-    }
+resource "aws_iam_role_policy_attachment" "tf-cicd-codebuild-attachment1" {
+    policy_arn  = aws_iam_policy.tf-cicd-build-policy.arn
+    role        = aws_iam_role.tf-codebuild-role.id
+}
 
+resource "aws_iam_role_policy_attachment" "tf-cicd-codebuild-attachment2" {
+    policy_arn  = "arn:aws:iam::aws:policy/PowerUserAccess"
+    role        = aws_iam_role.tf-codebuild-role.id
 }
